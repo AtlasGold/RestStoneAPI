5a929967e49a0f6546be46dee693ca086838bdc6
AtlasGold <luan.vini31@hotmail.com>
2022-12-17 18:10:57 -0300

    feat: added Query for better search

---
+++ model/ModelQuery.py
@@ -0,0 +1,12 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+
+
+class QueryMessage(BaseModel):
+    id: Optional[int]
+    text: Optional[str]
+    votes: Optional[int]
+
+
+class QueryUpdate(BaseModel):
+    id: Optional[int]
