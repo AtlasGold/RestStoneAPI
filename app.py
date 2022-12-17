7cdc6b4b42f16b5e4ac5a84ede8f919e6d43ba7a
AtlasGold <luan.vini31@hotmail.com>
2022-12-17 15:36:43 -0300

    feat: added data persistance

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
-from flask import Flask , request
+from flask import Flask ,request, jsonify
 from flask_pydantic_spec import FlaskPydanticSpec,  Response, Request
 from model.modelMessage import Message
+from tinydb import TinyDB, Query
 
 server =  Flask(__name__)
 spec = FlaskPydanticSpec("flask", title= "RestfullStone API")
 spec.register(server)
+database = TinyDB('database.json')
 
 
 
 @server.get('/message')
 def get_message():
-    return "Parede Falsa a Frente"
+    ''' Get All Saved Messages. '''
+    Query().all
+    return database.all()
 
 @server.post('/message')
 @spec.validate(body=Request(Message), resp=Response(HTTP_200=Message))
 def create_message():
-    ''' Add a New Message '''
+    ''' Add a New Message. '''
     body = request.context.body.dict()  
+    database.insert(body)
     return body
 
 server.run()
