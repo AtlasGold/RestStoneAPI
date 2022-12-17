7ea25db4173e44cedc681b9c2edf5d5f5d1945ba
AtlasGold <luan.vini31@hotmail.com>
2022-12-17 15:26:00 -0300

    feat: added post route

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask 
-from flask_pydantic_spec import FlaskPydanticSpec,  Response
+from flask import Flask , request
+from flask_pydantic_spec import FlaskPydanticSpec,  Response, Request
 from model.modelMessage import Message
 
 server =  Flask(__name__)
 spec = FlaskPydanticSpec("flask", title= "RestfullStone API")
 spec.register(server)
 
+
+
 @server.get('/message')
-@spec.validate(resp=Response(HTTP_200=Message))
-def pegar_pessoa():
+def get_message():
     return "Parede Falsa a Frente"
+
+@server.post('/message')
+@spec.validate(body=Request(Message), resp=Response(HTTP_200=Message))
+def create_message():
+    ''' Add a New Message '''
+    body = request.context.body.dict()  
+    return body
 
 server.run()
