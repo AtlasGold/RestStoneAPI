d06797a030269c8600a93d4a9ec54422e3d31b9a
AtlasGold <luan.vini31@hotmail.com>
2022-12-17 14:28:38 -0300

    feat: added message model

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask 
+from flask_pydantic_spec import FlaskPydanticSpec,  Response
+from model.modelMessage import Message
 
 server =  Flask(__name__)
+spec = FlaskPydanticSpec("flask", title= "RestfullStone API")
+spec.register(server)
 
-@server.get('/pessoas')
+@server.get('/message')
+@spec.validate(resp=Response(HTTP_200=Message))
 def pegar_pessoa():
-    return "teste"
+    return "Parede Falsa a Frente"
 
 server.run()
