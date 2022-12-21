7e203caa13a921179e0ddb45199c2ab0c28142ec
AtlasGold <luan.vini31@hotmail.com>
2022-12-21 14:03:12 -0300

    feat: adding new util functions for help testing

--- test/utils.py
+++ test/utils.py
@@ -1,7 +1,26 @@
 import random
 import string
+from httpx import delete, get, patch
+
 
 BASE_URL = "http://127.0.0.1:1234/messages"
+
+
+def DatabaseSize():
+    request = get(BASE_URL)
+    response = request.json()["count"]
+    return response
+
+
+def ClearDatabase():
+    for i in range(DatabaseSize()):
+        delete(BASE_URL + f"/{i}")
+
+
+def MakeVotes(votes_number: int, message_id: int):
+    for i in range(votes_number):
+        patch(BASE_URL + f"/{message_id}", json={"id": message_id})
+        i += 1
 
 
 def RandomChar():
