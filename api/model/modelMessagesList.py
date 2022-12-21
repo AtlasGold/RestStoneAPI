1ed0fc72d78b54f63ebf4ad57fa14ba99a552583
AtlasGold <luan.vini31@hotmail.com>
2022-12-21 01:14:11 -0300

    chore: fixing formating with black

--- api/model/modelMessagesList.py
+++ api/model/modelMessagesList.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from typing import List
 from api.model.modelMessage import MessageOut
 
+
 class Messages(BaseModel):
     Messages: list[MessageOut]
     count: int
--- api/schema/database.py
+++ api/schema/database.py
@@ -1,4 +1,4 @@
-from tinydb import TinyDB, Query
+from tinydb import TinyDB
 from tinydb.storages import MemoryStorage
 
 database = TinyDB("database.json")
